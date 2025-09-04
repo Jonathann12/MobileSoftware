12940aaa6a949b4faf4702addbfecbdb5f627b66
Jonathan <jonathanfreire.silva@hotmail.com>
2025-09-03 23:38:42 -0300

    Exercicios

---
+++ src/01-sequential/soma.ts
@@ -0,0 +1,10 @@
+import PromptSync from "prompt-sync";
+const prompt = PromptSync();
+
+
+const x = Number(prompt("Digite o valor de X: "));
+const y = Number(prompt("Digite o valor de Y: "));
+
+let soma = x + y;
+
+console.log("Soma = " + soma);
---
+++ src/04-Arrays/maiorPosicao.ts
@@ -0,0 +1,19 @@
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
+
+const num = Number(prompt("Quanto numeros voce vai digitar? "));
+
+const nume: Number[] = []; 
+
+for(let i = 0; i < num; i++){
+
+    const n = Number(prompt("Digite um numero: "));
+
+    
+} 
+
+
+
+
+
+
